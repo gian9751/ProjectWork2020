46576cf59ca736ea1dfdbed3675846f37ba4675c
gian9751 <lanagianluca81@gmail.com>
2020-04-29 09:55:19 +0200

    Aggiunta la versione landScape del dialog adapter

--- AndroidProject/app/src/main/java/com/example/androidproject/adapter/DialogAdapter.java
+++ AndroidProject/app/src/main/java/com/example/androidproject/adapter/DialogAdapter.java
@@ -4,7 +4,6 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,7 +22,6 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.example.androidproject.R;
 import com.example.androidproject.activity.MovieDetail;
-import com.example.androidproject.localdata.FavouritesTableHelper;
 import com.example.androidproject.localdata.MovieTableHelper;
 import com.example.androidproject.localdata.Provider;
 
@@ -56,7 +54,7 @@ public class DialogAdapter extends CursorAdapter {
     public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
 
            LayoutInflater vInflater = LayoutInflater.from(context);
-           View vView = vInflater.inflate(R.layout.dialog_item_favorite, viewGroup, false);
+           View vView = vInflater.inflate(R.layout.dialog_item_favourite, viewGroup, false);
 
            return vView;
     }
@@ -78,11 +76,20 @@ public class DialogAdapter extends CursorAdapter {
         mRequestOptions = new RequestOptions();
         mRequestOptions.placeholder(R.drawable.ic_movie_placeholder);
 
-        Glide
-                .with(context)
-                .setDefaultRequestOptions(mRequestOptions)
-                .load(cursor.getString(cursor.getColumnIndex(MovieTableHelper.POSTER_PATH)))
-                .into(vViewHolder.mImmagine);
+        if (context.getResources().getConfiguration().orientation==1) {
+            Glide
+                    .with(context)
+                    .setDefaultRequestOptions(mRequestOptions)
+                    .load(cursor.getString(cursor.getColumnIndex(MovieTableHelper.POSTER_PATH)))
+                    .into(vViewHolder.mImmagine);
+        }else {
+            Glide
+                    .with(context)
+                    .setDefaultRequestOptions(mRequestOptions)
+                    .load(cursor.getString(cursor.getColumnIndex(MovieTableHelper.BACKDROP_PATH)))
+                    .into(vViewHolder.mImmagine);
+        }
+
 
         vViewHolder.mTitle.setText(cursor.getString(cursor.getColumnIndex(MovieTableHelper.TITLE)));
 
@@ -110,5 +117,11 @@ public class DialogAdapter extends CursorAdapter {
                 Toast.makeText(context, "Movie eliminato dai preferiti :)", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    private void setBindViewHorizontal(View view, Context context, Cursor cursor) {
+    }
+
+    private void setBindViewVertical(View view, Context context, Cursor cursor) {
     }
 }
