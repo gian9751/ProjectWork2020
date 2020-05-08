8c0d6087a89c4ab67f6b77ff9a730c6dcdf921cb
MattiaPell <47603639+MattiaPell@users.noreply.github.com>
2020-05-08 11:06:21 +0200

    semifix onde splash
    da fixare altezza onde

--- AndroidProject/app/src/main/java/com/example/androidproject/activity/Splash.java
+++ AndroidProject/app/src/main/java/com/example/androidproject/activity/Splash.java
@@ -1,68 +1,47 @@
 package com.example.androidproject.activity;
 
-
-
-import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.androidproject.R;
-import com.scwang.wave.MultiWaveHeader;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
 
-import java.io.IOException;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.androidproject.R;
+import com.example.androidproject.waves.WaveView;
 
 public class Splash extends AppCompatActivity {
 
-    MultiWaveHeader waveFooter;
+    private static final String TAG = Splash.class.getSimpleName();
+
+    private WaveView waveView;
     private static int splashTimeOut = 5000;
 
+    ImageView mImageLogo;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
         final Intent i = new Intent(Splash.this, Home.class);
 
-            //se la versione delle api sono >= 23 allora mostra la spalshscreen altrimenti salta direttamente a home per evitare outofmemory
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                setContentView(R.layout.activity_splashscreen);
-                waveFooter = findViewById(R.id.waveFooter);
-                wave();
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
+        waveView = findViewById(R.id.waveFooter);
+        waveView.addDefaultWaves(2, 1);
 
-                        startActivity(i);
-                        finish();
-                    }
-                }, splashTimeOut);
-            } else {
-                        startActivity(i);
+        mImageLogo = findViewById(R.id.logo);
+        
+
+        waveView.startAnimation();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(i);
+                finish();
             }
+        }, splashTimeOut);
 
-
-
-
-
-
-
-
-
-
-
-    }
-
-    private void wave() {
-        waveFooter.setVelocity(2);
-        waveFooter.setProgress(1);
-        waveFooter.isRunning();
-        waveFooter.setGradientAngle(45);
-        waveFooter.setWaveHeight(70);
-        waveFooter.setStartColor(Color.BLUE);
-        waveFooter.setCloseColor(Color.CYAN);
     }
 }
